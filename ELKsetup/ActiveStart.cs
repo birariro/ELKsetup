86f6269906fef1c1b365612004cd86fcf9e25e4a
k4keye <vps32@naver.com>
2020-10-08 22:55:01 +0900

    kibana bug patch

--- ELKsetup/ActiveStart.cs
+++ ELKsetup/ActiveStart.cs
@@ -13,46 +13,48 @@ namespace ELKsetup
         {
             string[] checkList = new string[] { "elasticsearch", "logstash", "kibana", "filebeat" };
 
-            for(byte i = 0; i < checkList.Length; i++)
+            for(byte i = 0; i < checkList.Length; i++) //각 모든 설치파일을 동작시킨다.
             {
 
                 bool breakFlag = true;
-                bool nameCheck = true;
-                do
+                //bool nameCheck = true;
+                do //실행 성공할때까지 반복
                 {
 
-                    Console.WriteLine($"{checkList[i]} 설정 재시도중");
+                    
                     TerminalCommand.Command($"sudo  service {checkList[i]} status > /etc/setting{checkList[i]}.txt ; exit");
-                    Thread.Sleep(1000);
+                    
                     string[] settingArr = File.ReadAllLines(@$"/etc/setting{checkList[i]}.txt");
 
 
                     for (int ii = 0; ii < settingArr.Length; ii++)
                     {
-                        if (settingArr[ii].Contains(checkList[i]))
+                        /*if (settingArr[ii].Contains(checkList[i])) // 이름이 맞는지 왜검사하지?
                         {
                             Console.WriteLine("> "+settingArr[ii]);
                             nameCheck = false;
-                        }
-                        if (settingArr[ii].Contains("Active: active (running)"))
+                        }*/
+                        if (settingArr[ii].Contains("Active: active (running)")) //실행중이라면 종료해라
                         {
-                            Console.WriteLine(">>" +settingArr[ii]);
+                            Console.WriteLine($"{checkList[i]} 실행 확인!");
                             breakFlag = false;
                             break;
                         }
                         else if(settingArr[ii].Contains("Active:"))
                         {
-                            nameCheck = true;
+                            //nameCheck = true;
                             breakFlag = true;
+                            Console.WriteLine($"{checkList[i]} 실행 재시도중");
                             TerminalCommand.Command($"sudo  service {checkList[i]} start && exit;");
+                            Thread.Sleep(8000); //실행후 8초 대기
                             break;
                         }
                         
                         
                     }
-                    Task.Delay(3000);
+                    
 
-                } while (breakFlag || nameCheck);
+                } while (breakFlag);
 
             }
         }
