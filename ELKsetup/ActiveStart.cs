bcd0a225506d4204798e3fb68166e4a135a66a96
k4keye <vps32@naver.com>
2020-11-28 13:08:34 +0900

    activte

--- ELKsetup/ActiveStart.cs
+++ ELKsetup/ActiveStart.cs
@@ -47,45 +47,7 @@ namespace ELKsetup
 
 
                 } while (breakFlag);
-               /* bool breakFlag = true;
-                //bool nameCheck = true;
-                do //실행 성공할때까지 반복
-                {
-
-
-                    TerminalCommand.Command($"sudo  service {checkList[i]} status > /etc/setting{checkList[i]}.txt ; exit");
-
-                    string[] settingArr = File.ReadAllLines(@$"/etc/setting{checkList[i]}.txt");
-
-
-                    for (int ii = 0; ii < settingArr.Length; ii++)
-                    {
-                        if (settingArr[ii].Contains(checkList[i])) // 이름이 맞는지 왜검사하지?
-                        {
-                            Console.WriteLine("> " + settingArr[ii]);
-                            nameCheck = false;
-                        }
-                        if (settingArr[ii].Contains("Active: active (running)")) //실행중이라면 종료해라
-                        {
-                            Console.WriteLine($"{checkList[i]} 실행 확인!");
-                            breakFlag = false;
-                            break;
-                        }
-                        else if (settingArr[ii].Contains("Active:"))
-                        {
-                            //nameCheck = true;
-                            breakFlag = true;
-                            Console.WriteLine($"{checkList[i]} 실행 재시도중");
-                            TerminalCommand.Command($"sudo  service {checkList[i]} start && exit;");
-                            Thread.Sleep(5000); //실행후 5초 대기
-                            break;
-                        }
-
-
-                    }
-
-
-                } while (breakFlag);*/
+              
 
             }
         }
